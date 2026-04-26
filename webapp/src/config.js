8006e92f6b8e0b545405ea47fd0edcc2a96b6bb7
Glenn Bech <glenn.bech@gmail.com>
2026-04-26 10:18:20 +0000

    example url

--- webapp/src/config.js
+++ webapp/src/config.js
@@ -1,6 +1,6 @@
 // Configuration
 // Students will update this with their Lambda Function URL from Terraform output
-export const LAMBDA_URL = 'YOUR_LAMBDA_FUNCTION_URL_HERE';
+export const LAMBDA_URL = 'https://log7ry6sgkizltakiedbqnftxe0jmelz.lambda-url.eu-north-1.on.aws/';
 
 // For ToxiProxy experiments, change to:
 // export const LAMBDA_URL = 'http://localhost:8000';
