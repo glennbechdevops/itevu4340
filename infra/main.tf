b945946146865cba14cec98f00cbf55c1718da77
glennbech <glenn.bech@gmail.com>
2026-04-26 12:53:04 +0200

    Add source_arn to Lambda Function URL permission
    The permission resource needs to reference the Function URL ARN
    to properly grant InvokeFunctionUrl access for NONE auth type.

--- infra/main.tf
+++ infra/main.tf
@@ -139,9 +139,10 @@ resource "aws_lambda_function_url" "order_processor_url" {
 
 # Permission for Function URL to invoke Lambda
 resource "aws_lambda_permission" "function_url" {
-  statement_id           = "AllowFunctionURLInvoke"
+  statement_id           = "FunctionURLAllowPublicAccess"
   action                 = "lambda:InvokeFunctionUrl"
   function_name          = aws_lambda_function.order_processor.function_name
   principal              = "*"
   function_url_auth_type = "NONE"
+  source_arn             = aws_lambda_function_url.order_processor_url.function_arn
 }
